c651e65cd8238eea08b39b90f8ed930a5dcd0401
TauMare <sasas.sasas.972@gmail.com>
2023-04-27 01:42:18 +0700

    Доделана первичная верстка

---
+++ tasks/_constants.mjs
@@ -0,0 +1,33 @@
+const BUILD = "build";
+const SOURCE = "source";
+
+const PUG_DIR = `${SOURCE}/pages`;
+const DATA_DIR = `${SOURCE}/data`;
+
+export const Path = {
+	BUILD,
+	PUG: {
+		src: `${PUG_DIR}/*.pug`,
+		watch: `${PUG_DIR}/**/*.pug`,
+	},
+	DATA: {
+		src: DATA_DIR,
+		watch: `${DATA_DIR}/**/*.{yml, yaml}`,
+	},
+	ASSETS: `${SOURCE}/assets/**/*`,
+	STYLES: {
+		src: `${SOURCE}/sass/*.scss`,
+		dest: `${BUILD}/css`,
+		watch: `${SOURCE}/sass/**/*.scss`,
+		common: `${SOURCE}/sass/common/`,
+	},
+	SCRIPTS: {
+		src: `${SOURCE}/typescript/*.ts`,
+		dest: `${BUILD}/js`,
+		watch: `${SOURCE}/typescript/**/*.ts`,
+	},
+	IMAGES: {
+		src: `${SOURCE}/images/**/*`,
+		dest: `${BUILD}/img`,
+	}
+};
---
+++ tasks/serve.mjs
@@ -0,0 +1,38 @@
+import gulp from "gulp";
+import browserSync from "browser-sync";
+import { compilePug } from "./pug.mjs";
+import { Path } from "./_constants.mjs";
+import { copyAssets } from "./public.js";
+import { compileSass } from "./styles.mjs";
+import { compileScripts } from "./scripts.mjs";
+import { processImages } from "./images.mjs";
+
+const server = browserSync.create();
+
+/**
+ * @type {browserSync.Options}
+ */
+const SERVER_OPTIONS = {
+	server: Path.BUILD,
+	notify: true,
+	open: true,
+	cors: true,
+	watch: true,
+};
+
+const streamStyles = () => compileSass().pipe(server.stream());
+
+async function serve() {
+	server.init(SERVER_OPTIONS);
+
+	gulp.watch([Path.PUG.watch, Path.DATA.watch], compilePug);
+	gulp.watch(Path.ASSETS, copyAssets);
+	gulp.watch(Path.STYLES.watch, streamStyles);
+	gulp.watch(Path.SCRIPTS.watch, compileScripts);
+	gulp.watch(Path.IMAGES.src, processImages);
+}
+
+const initialBuild = gulp.parallel(compilePug, copyAssets, compileSass, compileScripts, processImages);
+const startServer = gulp.series(initialBuild, serve);
+
+export { initialBuild, startServer };
